5e2b06e6a1361085e41d8d5fadb8cae19028e81b
kyleoakes <koakes@csumb.edu>
2021-05-11 20:18:55 -0700

    initial commit

---
+++ src/main/java/cst438/Cst438Hw2Application.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Hw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Hw2Application.class, args);
+	}
+
+}
